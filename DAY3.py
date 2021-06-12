12def918a377df0f3c0cb03135db43cdd1bc83d5
senthil08012000 <67067751+senthil08012000@users.noreply.github.com>
2021-06-12 10:12:29 +0530

    Add files via upload

---
+++ DAY3.py
@@ -0,0 +1,27 @@
+#AGE CALCULATOR
+print("----welcome to age calculator----")
+year=int(input("enter the yar of birth"))
+count=0
+for i in range(year,2021):
+    count+=1
+print("your age is :",count)
+#SIMPLE CALCULATOR
+print()
+print()
+print("---welcome to simple calculator---")
+
+num1=int(input("enter the no 1: "))
+num2=int(input("enter the no 2: "))
+addition=num1+num2
+subtraction=num1-num2
+division=num1/num2
+floordivision=num1//num2
+multiplication=num1*num2
+power=num1**num2
+print("addition:",addition)
+print("subtraction:",subtraction)
+print("division:",division)
+print("floordivision:",floordivision)
+print("multiplication:",multiplication)
+print("power:",power)
+
